5c96c801b3c6a6322b8597c4c4de44e5d1e1d204
Sherif Elkhouly <sherif.elkhouly@clearmatics.com>
2021-08-22 16:16:05 +0100

    Setup changes committed

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ module.exports = {
   entry: path.join(__dirname, 'src/js', 'App.js'),
   devServer: {
     contentBase: path.join(__dirname, 'src'),
+    port: 3000
   },
   output: {
     path: path.join(__dirname, 'dist'),
